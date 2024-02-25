247a00c1cf0b2e0ac7e381ec201ec2e015f1a0fd
DonAllenII <don.a.alexanderii@gmail.com>
2024-02-25 16:06:49 -0500

    project map

---
+++ pokemon_api.py
@@ -0,0 +1,17 @@
+#connect to the pokemon_api
+
+#assign pokemon_api variable to url of complete pokemon_list
+
+#define using pokemon_api, pokemon as inputs; use string concat and variable to ouput url for a specific pokemon 
+
+#def using pokemon_api as an input, retrieve  of all pokemon names from pokemon_api using limiters, outputs a list pokemon_names
+    #define pokemon_api url for all pokemon
+
+for pokemon in pokedex:
+    list.append()
+#def retrieve type, name, abilities, weight
+
+#def using pokemon_names as an input; 20 as default input of amount of pokemon names returned,  return a list of 20 pokemon names
+
+
+#create a dictionary of pokemon by type: {name:  {ablities: [], weight:}
